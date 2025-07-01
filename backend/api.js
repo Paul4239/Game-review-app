71844c555a3aff69e8b987005a3921c0f5c6825b
Paul <ppd1512@gmail.com>
2025-07-01 16:20:00 +0100

    Created database and table on neondb and implemented it in api.js

--- backend/api.js
+++ backend/api.js
@@ -1,14 +1,34 @@
 const express = require("express");
 const cors = require("cors");
+const { Client } = require("pg");
+require("dotenv").config();
 const app = express();
 const port = 4000;
 
 app.use(cors());
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from the API!" });
+app.get("/reviews", async (req, res) => {
+  const client = new Client({
+    user: process.env.USER,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: process.env.PORT,
+    ssl: { rejectUnauthorized: false },
+  });
+
+  try {
+    await client.connect();
+    const result = await client.query("SELECT * FROM reviews;");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Database error" });
+  } finally {
+    await client.end();
+  }
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Database link: http://localhost:${port}/reviews`);
 });
